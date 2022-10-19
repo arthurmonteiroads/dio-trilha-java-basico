de0fabb4c3e5a30ebba2dfd6b0d17313587b39f2
arthurmonteiroads <arthurmonteiroads@outlook.com>
2022-10-19 16:08:46 -0300

    commit 2

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,23 @@
+public class Operadores {
+    public static void main(String[] args){
+        boolean condicao1=false;
+        boolean condicao2=false;
+
+        if(condicao1 && condicao2 ){
+            System.out.println("as duas condições são verdadeiras");
+        }
+        if(condicao1 || condicao2){
+            System.out.println("uma das concições é verdadeira");
+        }
+
+
+
+
+
+
+
+
+    }
+
+} 
+        
